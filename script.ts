b6f77a55a715c827e76f9ab916ff4e9cb5feba41
Seudonym <wk170179@gmail.com>
2023-12-15 14:33:46 +0530

    add mouse input

--- script.ts
+++ script.ts
@@ -4,6 +4,8 @@ function main() {
   const canvas = document.getElementById("canvas") as HTMLCanvasElement;
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+  canvas.style.width = window.innerWidth + "px";
+  canvas.style.height =window.innerHeight + "px";
   const gl = canvas.getContext("webgl");
   if (!gl) {
     throw new Error("WebGL not supported");
@@ -19,7 +21,7 @@ function main() {
   `;
 
   const fragmentShaderSource = `
-    precision mediump float; 
+    precision highp float; 
 
     uniform vec2 resolution;
     uniform int iterations;
@@ -51,8 +53,15 @@ function main() {
     void main() {
       vec2 uv = gl_FragCoord.xy / resolution.xy - vec2(0.5);
       uv *= 2.0 * vec2(resolution.x / resolution.y, 1.0);
-      vec3 color = mandelbrot(uv);
-      gl_FragColor = vec4(color, 1);
+      vec4 color = vec4(mandelbrot(uv / zoom + center), 1.0);
+      float absx = abs(uv.x);
+      float absy = abs(uv.y);
+      // Crosshair
+      if ((absx < 0.002 && absy < 0.02) || (absy < 0.002 && absx < 0.02)) {
+        color = vec4(1.0, 0.0, 0.0, 1.0);
+      }
+
+      gl_FragColor = color;
     }
   `;
 
@@ -66,11 +75,46 @@ function main() {
     uniformLocations: {
       resolution: gl.getUniformLocation(shaderProgram, "resolution"),
       iterations: gl.getUniformLocation(shaderProgram, "iterations"),
+      zoom: gl.getUniformLocation(shaderProgram, "zoom"),
+      center: gl.getUniformLocation(shaderProgram, "center"),
     },
   };
 
+  const uniforms = {
+    resolution: [canvas.width, canvas.height],
+    iterations: 100,
+    zoom: 1.0,
+    center: [0.0, 0.0],
+  };
+
   const vbo = initVBO(gl);
-  render(gl, vbo, programInfo);
+
+  document.addEventListener("wheel", (e) => {
+    e.preventDefault();
+    uniforms.zoom *= e.deltaY > 0 ? 1.05 : 1 / 1.05;
+    render(gl, vbo, programInfo, uniforms);
+  });
+
+  let held = false;
+  document.addEventListener("mousemove", (e) => {
+    if (!held) return;
+    console.log(e.movementX, e.movementY);
+    const x = canvas.width / canvas.height * (e.movementX / canvas.width);
+    const y = (e.movementY / canvas.height);
+
+    uniforms.center[0] -= x / uniforms.zoom;
+    uniforms.center[1] += y / uniforms.zoom;
+    render(gl, vbo, programInfo, uniforms);
+  });
+
+  document.addEventListener("mousedown", (e) => {
+    held = true;
+  });
+  document.addEventListener("mouseup", (e) => {
+    held = false;
+  });
+
+  render(gl, vbo, programInfo, uniforms);
 }
 
 function createShaderProgram(
@@ -135,7 +179,7 @@ function initVBO(gl: WebGLRenderingContext) {
   return vbo;
 }
 
-function render(gl: WebGLRenderingContext, vbo: WebGLBuffer, programInfo: any) {
+function render(gl: WebGLRenderingContext, vbo: WebGLBuffer, programInfo: any, uniforms: any) {
   gl.clearColor(0, 0, 0, 1);
   gl.clear(gl.COLOR_BUFFER_BIT);
   gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
@@ -144,5 +188,7 @@ function render(gl: WebGLRenderingContext, vbo: WebGLBuffer, programInfo: any) {
   gl.enableVertexAttribArray(programInfo.attribLocations.position);
   gl.uniform2f(programInfo.uniformLocations.resolution, programInfo.canvas.width, programInfo.canvas.height);
   gl.uniform1i(programInfo.uniformLocations.iterations, 100);
+  gl.uniform1f(programInfo.uniformLocations.zoom, uniforms.zoom);
+  gl.uniform2f(programInfo.uniformLocations.center, uniforms.center[0], uniforms.center[1]);
   gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 }
